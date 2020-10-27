2436824b48aadfb6e398fbd38a100af9001054be
Ashok sharma <ashokonmi3@gmail.com>
2020-10-27 20:56:23 +0530

    commit message

--- Selenium_Programs/src/Resource/FileUpload.au3
+++
@@ -1,6 +0,0 @@
-ControlFocus("Open","","Edit1" )
-Sleep(3000)
-ControlSetText("Open","","Edit1","C:\My\Ashok_Sharma.doc")
-Sleep(3000)
-
-ControlClick("Open","","Button1")
---
+++ firstjavaproject/src/firstjavaprojectpackage/firstclass.java
@@ -0,0 +1,10 @@
+package firstjavaprojectpackage;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
